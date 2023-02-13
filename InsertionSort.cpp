294db712585c281736ed5f052860a8949fab1730
Mohiuddinr22 <mohiuddinr.22@gmail.com>
2023-02-13 22:37:23 +0600

    git init

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -2,9 +2,21 @@
 #include <vector>
 using namespace std;
 
-void InsertionSort(vector<int> &arr, int size)
+void InsertionSort(int *arr, int size)
 {
-    
+    int i, j, k, carry;
+    for (i = 1; i < sizeof(arr); i++)
+    {
+        carry = arr[i];
+        for (j = i - 1; j < sizeof(arr); j--)
+        {
+            if (arr[j] < arr[i] && i != j)
+            {
+                carry = arr[j];
+            }
+        }
+        swap(carry, arr[i]);
+    }
 }
 
 int main()
