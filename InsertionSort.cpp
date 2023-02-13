6e46b707c52778fb5780dbf45e50dab2afd3cd5a
Mohiuddinr22 <mohiuddinr.22@gmail.com>
2023-02-13 22:27:37 +0600

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,16 +4,7 @@ using namespace std;
 
 void InsertionSort(vector<int> &arr, int size)
 {
-    int shift = 0, comp = 0, startingIndex, elementOnHand, currentIndex;
-    for (startingIndex = 1; startingIndex < size; startingIndex++)
-    {
-        elementOnHand = arr[startingIndex];
-        currentIndex = startingIndex - 1;
-
-        while ()
-        {
-        }
-    }
+    
 }
 
 int main()
