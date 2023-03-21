e9bbaa9bf322499919a90dfca5dc6d4c45dc4e97
Mohiuddinr22 <mohiuddinr.22@gmail.com>
2023-03-21 16:51:32 +0600

    git init

--- Graph2.cpp
+++ Graph2.cpp
@@ -7,9 +7,11 @@ private:
     int numVertices;
     int **adjMatrix;
     bool *visited;
+    vector<int> visitOrder;
     void DFS_visit(int v)
     {
         visited[v] = true;
+        visitOrder.push_back(v);
         for (int i = 0; i <= numVertices; i++)
         {
             if (i != v && adjMatrix[v][i] == 1 && visited[i] == false)
@@ -45,8 +47,15 @@ public:
     }
     void removeEdge(int from, int to)
     {
-        adjMatrix[from][to] = 0;
-        adjMatrix[to][from] = 0;
+        if (from > numVertices)
+            cout << from << " does not exist in your graph" << endl;
+        if (to > numVertices)
+            cout << to << " does not exist in your graph" << endl;
+        else
+        {
+            adjMatrix[from][to] = 0;
+            adjMatrix[to][from] = 0;
+        }
     }
     void showAdjMatrix()
     {
@@ -64,10 +73,9 @@ public:
     void DFS(int v)
     {
         DFS_visit(v);
-        for (int i = 0; i <= numVertices; i++)
+        for (int i = 0; i < visitOrder.size(); i++)
         {
-            if (visited[i] == true)
-                cout << i << "\t";
+            cout << visitOrder[i] << "\t";
         }
         cout << endl;
     }
@@ -75,20 +83,16 @@ public:
 
 int main()
 {
-    Graph graph(10);
+    Graph graph(9);
     graph.addEdge(1, 2);
     graph.addEdge(1, 3);
-    graph.addEdge(2, 6);
-    graph.addEdge(3, 6);
-    graph.addEdge(3, 5);
-    graph.addEdge(6, 7);
+    graph.addEdge(2, 5);
+    graph.addEdge(2, 7);
+    graph.addEdge(3, 4);
+    graph.addEdge(3, 9);
+    graph.addEdge(9, 8);
     graph.addEdge(6, 8);
-    graph.addEdge(8, 9);
-    graph.addEdge(9, 10);
-    graph.addEdge(4, 10);
-    graph.addEdge(4, 5);
-    graph.addEdge(5, 9);
-    graph.removeEdge(6, 7);
-    graph.showAdjMatrix();
+    graph.addEdge(5, 6);
+    // graph.showAdjMatrix();
     graph.DFS(1);
 }
